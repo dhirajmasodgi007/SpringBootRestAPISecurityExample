c855092ab0011a05bd6f72ffa0a842c02618ac13
DhirajKM <dhirajmasodgi007@gmail.com>
2017-04-05 06:42:15 +1000

    Method Name correction.

--- SpringBootRestAPISecurityExample/src/test/java/com/example/SpringRootRestApiSecurityExampleApplicationClient.java
+++ SpringBootRestAPISecurityExample/src/test/java/com/example/SpringRootRestApiSecurityExampleApplicationClient.java
@@ -1,12 +1,16 @@
 package com.example;
 
+import java.nio.charset.Charset;
 import java.util.Arrays;
 import java.util.LinkedHashMap;
 import java.util.List;
 
 import org.apache.tomcat.util.codec.binary.Base64;
+import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.RestTemplate;
 
 import com.spring.security.restful.springboot.model.WordCount;
@@ -37,17 +41,46 @@ public class SpringRootRestApiSecurityExampleApplicationClient {
 	    }
 	    
 	    
+	    private static HttpHeaders createHeaders(String username, String password){
+	    	   return new HttpHeaders() {{
+	    	         String auth = username + ":" + password;
+	    	         byte[] encodedAuth = Base64.encodeBase64( 
+	    	            auth.getBytes(Charset.forName("US-ASCII")) );
+	    	         String authHeader = "Basic " + new String( encodedAuth );
+	    	         set( "Authorization", authHeader );
+	    	      }};
+	    	}
+	    
+	    
+
+		 /* GET */
+		    @SuppressWarnings("unchecked")
+		    private static void findWordCountWithAuthentication(){
+		        System.out.println("Begin findWordCount API");
+		          
+		        RestTemplate restTemplate = new RestTemplate();
+		   //     List<WordCount> wordCountList = restTemplate.getForObject(REST_SERVICE_URI+"/search/"  + "Duis,Sed,Donec,Augue,Pellentesque" , List.class);
+		          
+		        ResponseEntity<List> response = restTemplate.exchange(REST_SERVICE_URI+"/search/"  + "Duis,Sed,Donec,Augue,Pellentesque", HttpMethod.GET, new HttpEntity<Object>(createHeaders("abcde", "fghij")), List.class);
+		        
+		        System.out.println("findWordCountWithAuthentication result =" + response.getBody());
+		       /* if(null != wordCountList && wordCountList.size() > 0)
+		        {
+		            for(WordCount wc : wordCountList)
+		            {
+		                System.out.println("Word= " + wc.getWord() +", Count=" + wc.getCount()+ "\n");;
+		            }
+		        }*/
+		       
+		    }
+	    
+	    
 	    /* GET */
 	    @SuppressWarnings("unchecked")
 	    private static void topWordCountWithoutAuthentication(){
 	        System.out.println("Begin topWordCount API");
 	          
 	        RestTemplate restTemplate = new RestTemplate();
-	        /*if (needsProxy()) {
-	            System.setProperty("http.proxyHost", getProxyHost());
-	            System.setProperty"http.proxyPort", getProxyPort());
-	        } */
-	        
 	        List<WordCount> wordCountList = restTemplate.getForObject(REST_SERVICE_URI+"/top/5", List.class);
 	          
 	        if(null != wordCountList && wordCountList.size() > 0)
@@ -74,6 +107,10 @@ public class SpringRootRestApiSecurityExampleApplicationClient {
 	    
 	public static void main(String[] args) {
 	
+		
+		findWordCountWithAuthentication();
+		
+		/*  Below method are for without authentication */
 			topWordCountWithoutAuthentication();
 			findWordCountWithoutAuthentication();
 			
